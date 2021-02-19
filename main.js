283156f341ea5434cafadc3476cdd9a0b21cc283
gitjdeng <pjdben@foxmail.com>
2021-02-19 16:44:18 +0800

    test

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-//import './example';
-//import {example_1} from './example';
-let { start_app, click_item, set_volume, wait_befor_click,wait_for } = require('lib.js');
+events.observeToast();
 
+auto();
 toast('start test');
 launch('com.sandboxol.blockymods');
 sleep(5000);
@@ -9,9 +8,8 @@ sleep(5000);
 var text = "test_start";
 files.write("/sdcard/text_result.txt", text);
 
-function example_1(Authority){
     //miui_12
-        if(id("parentPanel").exists()){
+        /* if(id("parentPanel").exists()){
             id("permission_allow_foreground_only_button").click();
             sleep(10000);
             if(className("android.widget.LinearLayout").depth(1).exists()){
@@ -26,15 +24,27 @@ function example_1(Authority){
         }
         else sleep(3000);
         return;
+    } */
+    //miui_10
+    if(className("android.widget.FrameLayout").test("要允许 Blockman Go 访问以下权限吗？").exists()){
+        className("android.widget.Button").test("允许").findOne().click();
+        sleep(10000);
+        if(className("android.widget.LinearLayout").depth(1).exists()){
+            var text = "\n测试权限申请 pass";
+            files.append("/sdcard/text_result.txt", text);
+        }
+        else
+        {
+            var text = "\n测试权限申请 false";
+            files.append("/sdcard/text_result.txt", text);
+        }
     }
-
-var example_1 = new example_1(true);
+    else sleep(3000);
 
 if(className("android.widget.LinearLayout").depth(1).exists()){
     id("btn_sure").findOne().click();
     wait_fort('注册成功');
-    
-    if(ture){
+    if(tosat == '注册成功'){
         var text = "\n游客账号创建 pass";
         files.append("/sdcard/text_result.txt", text);
     }
