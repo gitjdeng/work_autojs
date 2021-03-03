ec0c3b40c7989d408bcf44f5574070959279de7e
gitjdeng <pjdben@foxmail.com>
2021-03-03 21:07:53 +0800

    注册修改

--- main.js
+++ main.js
@@ -30,12 +30,12 @@ function example_1(){
         click(360,1162,702,1262);
         waitForActivity("com.sandboxol.blockymods.view.activity.main.MainActivity");
         if(className("android.view.ViewGroup").depth(11).drawingOrder(2).indexInParent(0).exists()){
-            var text = "\nexample_1 测试权限申请 pass";
+            var text = "\ntest_case1 读写测试权限申请 pass";
             files.append("/sdcard/text_result.txt", text);
         }
         else
         {
-            var text = "\nexample_1 测试权限申请 false";
+            var text = "\ntest_case1 读写测试权限申请 false";
             files.append("/sdcard/text_result.txt", text);
         }
     }
@@ -43,21 +43,70 @@ function example_1(){
 }
 example_1();
 
+function staAt(){
+    app.startActivity({
+        action: "View", 
+        packageName:"com.sandboxol.blockymods",
+        className: "com.sandboxol.login.view.activity.login.LoginActivity",
+        root: true
+    });
+    sleep(3000);
+}
+//注册账号
+/* staAt();
+id("tv_register").findOne().click();
+waitForActivity("com.sandboxol.login.view.fragment.register.RegisterActivity"); */
 app.startActivity({
     action: "View", 
     packageName:"com.sandboxol.blockymods",
-    className: "com.sandboxol.login.view.activity.login.LoginActivity",
+    className: "com.sandboxol.login.view.fragment.register.RegisterActivity",
     root: true
 });
 sleep(3000);
 
+var ssnn;
+function suiji(){
+    var ss = random(10,1000);
+    //var ssn = String.fromCharCode(ss);
+    var ssn = ss.toString();
+    return ssnn = ssn;
+}
+suiji();
+
+setText(0,"jdengnv");
+
+//setText(0,"test"+ ssnn);
+setText(1,"a112233");
+setText(2,"a112233");
+className("android.widget.Button").findOne().click();
+sleep(2000);
+//var text_1 = className("android.widget.FrameLayout").depth(10).findOne().getText();
+
+/* do()
+while */
+if(className("android.weiget.TextView").text("用户名已存在").findOne().parent().exists()){
+    suiji();
+    setText(0,"test"+ ssnn);
+    sleep(3000);
+    className("android.widget.Button").findOne().click();
+}
+/* else if(text_1 == ""){
+
+} */
+else sleep(1000);
+
+
+
 //登录页
+staAt();
 if(className("android.view.ViewGroup").depth(6).exists()){
-    id("editName").findOne().setText("1170088800");
-    click(60,472,660,572);
-    className("android.widget.FrameLayout").depth(8).drawingOrder(1).indexInParent(0).findOne().setText("jdeng123456");
+    //控件限制，暂时只能先全文本输入密码，再更改账号
+    setText(0,"1170088800");
+    setText(1,"jdeng123456");
+    //click(60,472,660,572);
+    //className("android.widget.FrameLayout").depth(8).drawingOrder(1).indexInParent(0).findOne().setText("jdeng123456");
     id("btn_sign").findOne().click();
-    sleep(2000);
+    sleep(3000);
 }
 else sleep(1000);
 
